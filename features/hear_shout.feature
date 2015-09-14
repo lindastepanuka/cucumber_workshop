7953c16e7e8b68bbb090b3e243b5d8411b9f415c
Linda Stepanuka <linda.stepanuka@tieto.com>
2015-09-14 09:21:27 +0300

    Added the exercise from 11-09-2015 workshop

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,3 +1,29 @@
 Feature: Hear Shout
 
-  A description goes here...
+  Stakeholders:
+    Some companies that want to advertise
+    People who want to share their thoughts
+
+  The typical users:
+    Company - a shouter
+    Mary - a shouter
+    Ann - a listener
+
+  Background:
+    Given Company and Ann are within system
+
+  Scenario Outline: Company shouts and Ann hears
+    And Company is at position <company_position> and Ann is in position <ann_position>
+    When Company shouts "FREE BDD WORKSHOP FOR ALL"
+    Then Ann should hear "FREE BDD WORKSHOP FOR ALL"
+
+  Examples:
+    |company_position|ann_position|
+    |0|100|
+    |0|1000|
+    |-100|200|
+
+  Scenario: Company shouts and Ann does not hear
+    And Company and Ann are not in range of each other
+    When Company shouts "FREE BDD WORKSHOP FOR ALL"
+    Then Ann should not hear "FREE BDD WORKSHOP FOR ALL"
--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -2,4 +2,49 @@ var chai   = require('chai');
 var expect = chai.expect;
 var Shouty = require('../../lib/shouty');
 
-module.exports = function () {};
+var Network = Shouty.Network;
+var SystemUser = Shouty.SystemUser;
+
+module.exports = function () {
+
+  this.Given(/^Company and Ann are within system$/, function (callback) {
+    this.network = new Network();
+
+    this.Company = new SystemUser(this.network);
+    this.Ann = new SystemUser(this.network);
+
+    callback();
+  });
+
+  this.Given(/^Company is at position (.*) and Ann is in position (.*)$/, function (companyPosition, annPosition, callback) {
+    this.Company.position = companyPosition;
+    this.Ann.position = annPosition;
+
+    callback();
+  });
+
+  this.When(/^Company shouts "([^"]*)"$/, function (shout, callback) {
+    this.Company.shout(shout);
+    // Write code here that turns the phrase above into concrete actions
+    callback();
+  });
+
+  this.Then(/^Ann should hear "([^"]*)"$/, function (shout, callback) {
+    expect(this.Ann.heardMessages).to.include(shout);
+
+    callback();
+  });
+
+  this.Given(/^Company and Ann are not in range of each other$/, function (callback) {
+    this.Company.position = 0;
+    this.Ann.position = 1001;
+
+    callback();
+  });
+
+  this.Then(/^Ann should not hear "([^"]*)"$/, function (shout, callback) {
+    expect(this.Ann.heardMessages).to.not.include(shout)
+
+    callback();
+  });
+};
